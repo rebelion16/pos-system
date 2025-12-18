4c1c2aece2b02dce16cd26173ee899165e9c391e
Lukmanul Khakim <lukmanandika11@gmail.com>
2025-12-19 04:28:22 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,11 @@ export const metadata: Metadata = {
   title: 'POS UMKM - Sistem Point of Sale',
   description: 'Sistem Point of Sale (POS) lengkap untuk Usaha Mikro, Kecil, dan Menengah (UMKM)',
   keywords: ['POS', 'UMKM', 'Point of Sale', 'Kasir', 'Toko'],
+  icons: {
+    icon: 'https://i.ibb.co/Q36sR6H8/online-shop-1200175.png',
+    shortcut: 'https://i.ibb.co/Q36sR6H8/online-shop-1200175.png',
+    apple: 'https://i.ibb.co/Q36sR6H8/online-shop-1200175.png',
+  },
 }
 
 export default function RootLayout({
